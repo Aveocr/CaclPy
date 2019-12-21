fc5231af0718078a6496208a55f106a05a6ec2db
DenisCompany <30878230+DenisCompany@users.noreply.github.com>
2019-12-21 20:00:36 +0400

    New version 2.0
    Absolute new code

--- calc.py
+++ calc.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python3
-from math import pow
+import numpy as np
+import os
 NULL = ''
-version = 0.1
+version = 0.2
 action = True
 
-
-# main unit
-def main():
-    print("Hello! This is my calc, which I was writting with Python. Version %f and Autor - Denis Mustafin" % version)
-    action = True
-    while action==True:
-        oneNumb = float(input("\nPlease, input your first number -> "))
-        twoNumb = float(input("\nPlease, input your second number -> "))
-        value = str(input("\nWhat do you want doing now? *, /, +, -. ^ or for exit exit --> "))
-        chooseValue = {
-		    '+': "\n%f+%f=%f\n " % (oneNumb, twoNumb, oneNumb+twoNumb),
-		    '-': "\n%f-%f=%f\n " % (oneNumb, twoNumb, oneNumb-twoNumb),
-		    '*': "\n%f*%f=%f\n " % (oneNumb, twoNumb, oneNumb*twoNumb),
-		    '/': "\n%f/%f=%f\n " % (oneNumb, twoNumb, oneNumb/twoNumb),
-		    '^': "\n%f*%f=%f\n " % (oneNumb, twoNumb, pow(oneNumb, twoNumb))
-	    }
-        if(value == 'exit'):
-            action = False
-            print("=(. goodluck you and I'm sleep!")
-        print(chooseValue.get(value))
-
-
-
-main()
+if __name__ == '__main__':
+	os.system("clear")
+	print("\t\t\tWelcome to Chiken Calc\t\t\t")
+	print("Version: ", version)
+	print("Autor: Denis Mustafin")
+	print("GitHub url: https://github.com/DenisCompany/CaclPy/edit/master/calc.py\n\n\n")
+	print("For cancel to press on 'ctrl' + 'c'")
+	action = True
+	while True:
+		# Обработка ввода 
+		try :
+			first_num = float(input("Please input first number ->"))
+			second_num = float(input("Please input second number ->"))
+			sing = input("This's calculat supports the following action ->\n +, -,*, /, mod, div, pow, gcd, lcm\n")
+		except KeyboardInterrupt:
+			# os.system("clear")
+			print("\n")
+			break
+		except ValueError: 
+			print("Repeat your entry again")
+			continue
+		# Произведение операции
+		try :
+			if (sing != 'gcd' and sing != 'lcm' ): 
+				act = ("first_num " + sing + " second_num").replace("mod", "%").replace("div", "//").replace("pow", "**")
+				if ("%" in act or "//" in act or "/" in act) and act == 0.0: print("Деление на 0!")
+			if   sing=="gcd": 
+				print(np.gcd(int(first_num), int(second_num))); continue 
+			elif sing=="lcm": print(np.lcm(int(first_num), int(second_num))); continue 
+			else: print(eval(eval("act")))	
+		except SyntaxError:
+			print("Maybe you didn't write the action correctly or you did choose function, what haven't in menu\nPlease again")
